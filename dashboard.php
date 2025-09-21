cc94f0fd3ac580f3fccd6c786ea680e28449ef98
himesh-mehta <himesh.mehta@somaiya.edu>
2025-09-21 13:23:41 +0530

    more updates

--- dashboard.php
+++ dashboard.php
@@ -324,11 +324,12 @@ $user_email = $_SESSION['user_email'] ?? "student@example.com";
       </main>
     </div>
 
-    <!-- <script src="js/data.js"></script>
+    <script src="js/data.js"></script>
     <script src="js/charts.js"></script>
     <script src="js/themes.js"></script>
-    <script src="js/auth.js"></script>
-    <script src="js/timer.js"></script>
-    <script src="js/app.js"></script> -->
+    <!-- <script src="js/auth.js"></script> -->
+     <script src="js/timer.js"></script> 
+    <script src="js/app.js"></script> 
   </body>
 </html>
+
